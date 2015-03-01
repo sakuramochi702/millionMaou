dcb774909d80602c5b78ecc0127994cfc24ec133
sakuramochi702 <sakuramochiampan@gmail.com>
2015-03-01 12:57:27 +0900

    reduce resource
    alter push to set

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-var milkcocoa = new MilkCocoa("https://io-ri6ltx43y.mlkcca.com/");
+var milkcocoa = new MilkCocoa("https://io-bi6p7ipvv.mlkcca.com/");
 /* your-app-id にアプリ作成時に発行される"io-"から始まるapp-idを記入します */
-var maoDataStore = milkcocoa.dataStore("mao");
+var maoDataStore = milkcocoa.dataStore("mao01");
 var imgMao, txtCount, pName, board;
+var maoId="";
 var remain=1000000;
 
 window.onload = function(){
@@ -17,6 +18,7 @@ function loadData() {
 	//query.limit(1000000);
 	query.done(function(data) {
 		data.forEach(function(value) {
+			maoId=value.id;
 			remain = Number(value.hp);
 		});
 		//var count = 0;
@@ -45,10 +47,15 @@ function clickEvent(){
 
 function attack(){
 	if (remain > 0) {
-		maoDataStore.push({name : pName, hp : String(remain-1)},
-			function(data){
-				console.log("送信OK!");
-			});
+		if (maoId.length == 0) {
+			maoDataStore.push({name : pName, hp : String(remain-1)},
+				function(data){
+					console.log("送信OK!");
+					maoId = data.id;
+				});
+		} else {
+			maoDataStore.set(maoId, {name : pName, hp : String(remain-1)});
+		}
 	} else {
 		var msgDom = document.createElement("p");
 		msgDom.innerHTML = "ミリオンまおうはすでに倒されている。";
@@ -57,8 +64,16 @@ function attack(){
 }
 
 maoDataStore.on("push",function(data){
-	//他の人が更新した場合もここで受け取れる
-	remain--;
+	//他の人がpushした場合もここで受け取れる
+	maoId = data.id;
+	remain = data.value.hp;
+	txtCount.innerHTML=String(remain);
+	addConsole(data.value.name);
+});
+
+maoDataStore.on("set",function(data){
+	//他の人がsetした場合もここで受け取れる
+	remain = data.value.hp;
 	txtCount.innerHTML=String(remain);
 	addConsole(data.value.name);
 });
